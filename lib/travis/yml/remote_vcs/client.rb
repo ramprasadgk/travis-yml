7fa1f33d029defda5e5ee3e9c4b8bad0c4b42811
Artur Kremens <artur.kremens@gmail.com>
2020-08-20 14:32:49 +0200

    Update middleware

--- lib/travis/yml/remote_vcs/client.rb
+++ lib/travis/yml/remote_vcs/client.rb
@@ -5,26 +5,28 @@ module Travis
     module RemoteVcs
       class Client
         private
-        
+
         def connection
           @connection ||= Faraday.new(http_options.merge(url: config[:vcs][:url])) do |c|
             c.request :authorization, :token, config[:vcs][:token]
             c.request :retry, max: 5, interval: 0.1, backoff_factor: 2
             c.request :json
-            c.use Faraday::Response::RaiseError
-            c.use Faraday::Adapter::NetHttp
+            c.use FaradayMiddleware::Instrumentation
+            c.request :retry
+            c.response :raise_error
+            c.adapter :net_http
           end
         end
-        
+
         def http_options
           { ssl: config[:ssl].to_h }
         end
-        
+
         def request(method, name)
           resp = connection.send(method) { |req| yield(req) }
           JSON.parse(resp.body)['data']
         end
-        
+
         def config
           Yml.config
         end
