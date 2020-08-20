ff399f34b5434306862ece7aad7693e9e91ba851
Artur Kremens <artur.kremens@gmail.com>
2020-08-20 13:48:19 +0200

    add logs

--- lib/travis/yml/configs/content.rb
+++ lib/travis/yml/configs/content.rb
@@ -6,43 +6,47 @@ module Travis
     module Configs
       class Content < Struct.new(:repo, :path, :ref)
         include Base64, Errors
-        
+
         def content
           @content ||= normalize(fetch)
         end
-        
+
         private
-        
+
         def fetch
-          puts "Fetch #{repo.inspect}"
+          logger.info "Fetch #{repo.inspect}"
           data = client.content(id: repo.id, path: path, ref: ref)
-          puts "received #{data}"
+          logger.info "received #{data}"
           decode64(data['content'].to_s)
         rescue Faraday::ClientError => e
           api_error('RemoteVcs', :file, [repo.slug, self.path].join(':'), OpenStruct.new(e.response))
         rescue TypeError => e
           nil
         end
-        
+
         NBSP = "\xC2\xA0".force_encoding('binary')
-        
+
         def normalize(str)
           return unless str
           str = normalize_nbsp(str)
           str = remove_utf8_bom(str)
           str
         end
-        
+
         def normalize_nbsp(str)
           str.gsub(/^(#{NBSP})+/) { |match| match.gsub(NBSP, ' ') }
         end
-        
+
         def remove_utf8_bom(str)
           str.force_encoding('UTF-8').sub /^\xEF\xBB\xBF/, ''
         end
-        
+
         def client
           @clien ||= ::Travis::Yml::RemoteVcs::Repository.new
+        end
+
+        def logger
+          Yml.logger
         end
       end
     end
